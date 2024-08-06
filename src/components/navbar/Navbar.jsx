74932396c4220f8c868f83b3b965ef646bc2e5c1
David <morabongs@naver.com>
2024-08-06 18:18:35 +0900

    Completing Sidebar Menu Link Animation.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,20 @@
+import Sidebar from "../sidebar/Sidebar";
+import { motion } from "framer-motion";
+
 import "./navbar.scss";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Sidebar */}
+      <Sidebar />
       <div className="wrapper">
-        <span>Lama Dev</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Lama Dev
+        </motion.span>
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="" />
