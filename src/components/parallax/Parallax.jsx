6a3163e4b385fa6e247de99c5fab59f0c9dee711
David <morabongs@naver.com>
2024-08-08 18:37:29 +0900

    Completing Services Section Design.

---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,56 @@
+/* 패럴랙스 효과: framer-motion의 useScroll과 useTransform을 사용하여 사용자가 페이지를 스크롤할 때 부드러운 패럴랙스 애니메이션을 구현합니다.
+   각기 다른 요소들이 서로 다른 속도로 움직이며 레이어의 깊이감을 만듭니다. */
+
+import { motion, useScroll, useTransform } from "framer-motion";
+
+import "./parallax.scss";
+import { useRef } from "react";
+
+const Parallax = ({ type }) => {
+  const ref = useRef();
+
+  // 스크롤의 진행 상태를 scrollYProgress로 제공합니다.
+  const { scrollYProgress } = useScroll({
+    // 스크롤 위치를 계산할 참조 요소입니다.
+    target: ref,
+    // 스크롤의 시작과 끝을 정의하는 설정입니다.
+    // 요소의 상단이 뷰포트의 상단과 만나면 스크롤 애니메이션이 시작됩니다.
+    // 요소의 하단이 뷰포트의 상단과 만나면 스크롤 애니메이션이 끝납니다.
+    offset: ["start start", "end start"],
+  });
+
+  // 스크롤 진행에 따라 motion.h1의 Y축 위치를 조절합니다.
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  // 스크롤 진행에 따라 배경과 다른 요소의 Y축 위치를 조절합니다.
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  return (
+    <div
+      className="parallax"
+      ref={ref}
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, #111132, #0c0c1d)"
+            : "linear-gradient(180deg, #111132, #505064)",
+      }}
+    >
+      <motion.h1 style={{ y: yText }}>
+        {type === "services" ? "What We Do?" : "What We Did?"}
+      </motion.h1>
+      <motion.div className="mountains" />
+      <motion.div
+        className="planets"
+        style={{
+          y: yBg,
+          backgroundImage: `url(${
+            type === "services" ? "/planets.png" : "/sun.png"
+          })`,
+        }}
+      />
+      <motion.div style={{ x: yBg }} className="stars" />
+    </div>
+  );
+};
+
+export default Parallax;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,124 @@
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
+
+import "./services.scss";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Services = () => {
+  const ref = useRef();
+  /* useInView:
+    - 뷰포트 진입 감지: 요소가 뷰포트에 들어올 때와 나갈 때를 감지합니다.
+    - 애니메이션 트리거: 요소가 뷰포트에 보일 때 특정 애니메이션이나 스타일 변경을 시작할 수 있습니다.
+    - 성능 최적화: 사용자가 볼 수 있는 요소에만 애니메이션을 적용하여 성능을 개선합니다.
+    
+    *margin: 요소가 뷰포트에 완전히 들어오기 100px 전에 isInView가 true로 설정되도록 만듭니다.
+  */
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      //   animate="animate"
+      //   whileInView="animate"
+      ref={ref}
+      animate={isInView && "animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I focus on helping your brand grow
+          <br /> and move forward
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{ color: "orange" }}>For Your</motion.b>{" "}
+            Business.
+          </h1>
+          <button>WHAT WE DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
